e2d78d285991ef59a1eb1690a5fbc0a75643892a
binners <binner29@gmail.com>
2014-11-04 13:16:10 -0500

    Create modules (guia de remision)
    transportista, vehiculo, condutor

---
+++ application/models/mod_conductor.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of mod_conductor
+ *
+ * @author JR
+ */
+class mod_conductor {
+    //put your code here
+}
